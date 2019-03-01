8bc2b0a780d069c95d702cb71c2bc45b3d05a61e
Zabih Alipour <za.alipour.java@gmail.com>
2019-03-01 22:45:34 +0330

    Initialize: - example 1 HelloWord         => Done - example 2 WorkQueues        => Done - example 3 Publish/Subscribe => Done - example 4 Routing           => Done

---
+++ Routing/src/main/java/EmitLogDirect.java
@@ -0,0 +1,22 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLogDirect {
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection()) {
+            Channel channel = connection.createChannel();
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+            String severity = argv[0];
+            String message = argv[1];
+
+            channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
+        }
+    }
+}
